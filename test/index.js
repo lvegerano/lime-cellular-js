108d445c45f5009ba545475f369f44a74064005a
Luis Vegerano <lvegerano422@gmail.com>
2018-08-08 19:51:19 -0500

    Adds generate api id api Adds opt-in numbers api

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
 const sinon = require('sinon');
+const { URL } = require('url');
 const proxyrequire = require('proxyquire');
 const mockRequire = require('mock-require');
 
@@ -116,4 +117,63 @@ describe('Lime Celullar SDK', function () {
     });
   });
 
+  describe('opt-in numbers api', function () {
+    let sdk;
+
+    before(function () {
+      const creds = {
+        ADMIN_API_ID: 'supersecretapikey',
+        ADMIN_USERNAME: 'luisvegerano',
+      };
+      sdk = lib(creds);
+    });
+
+    afterEach(function () {
+      sandbox.resetHistory();
+    });
+
+    it('defines a funtion optInNumbers', async function () {
+      const apiCall = new URL('https://mcpn.us/limeApi')
+      apiCall.searchParams.append('ev', 'optedInNumbers');
+      apiCall.searchParams.append('user', 'advertiserUserName');
+      apiCall.searchParams.append('api_id', 'advertiserApiId');
+      apiCall.searchParams.append('optInListId', 12323);
+      apiCall.searchParams.append('isWalletUrl', false);
+      apiCall.searchParams.append('isOnlyNumbers', false);
+
+      assert.isFunction(sdk.optInList.getByName);
+      await sdk.optInNumbers('advertiserUserName', 'advertiserApiId', 12323);
+      sinon.assert.calledOnce(fetchStub);
+      sinon.assert.calledWithExactly(fetchStub, apiCall.toString());
+    });
+  });
+
+  describe('generate api id', function () {
+    let sdk;
+
+    before(function () {
+      const creds = {
+        ADMIN_API_ID: 'supersecretapikey',
+        ADMIN_USERNAME: 'luisvegerano',
+      };
+      sdk = lib(creds);
+    });
+
+    afterEach(function () {
+      sandbox.resetHistory();
+    });
+
+    it('defines a funtion generateApiId', async function () {
+      const apiCall = new URL('https://mcpn.us/limeApi')
+      apiCall.searchParams.append('ev', 'generateApi');
+      apiCall.searchParams.append('user', 'adminUserName');
+      apiCall.searchParams.append('api_id', 'adminApiId');
+      apiCall.searchParams.append('account_username', 'advertiserUserName');
+
+      assert.isFunction(sdk.optInList.getByName);
+      await sdk.generateApiId('adminUserName', 'adminApiId', 'advertiserUserName');
+      sinon.assert.calledOnce(fetchStub);
+      sinon.assert.calledWithExactly(fetchStub, apiCall.toString());
+    });
+  });
 });
